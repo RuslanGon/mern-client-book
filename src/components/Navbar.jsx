9153fe48dbcb36615b876425469a4777fefdb699
RuslanGon <ruslan441006@gmail.com>
2025-03-21 13:27:17 +0200

    wer

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { FaBlog } from "react-icons/fa6";
+
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -17,21 +19,29 @@ const Navbar = () => {
         setIsSticky(false);
       }
     };
-    window.addEventListener('scroll', handleScroll)
+    window.addEventListener("scroll", handleScroll);
     return () => {
-    window.addEventListener('scroll', handleScroll) 
-    }
+      window.addEventListener("scroll", handleScroll);
+    };
   }, []);
 
- const navItems = [
-    {Link: 'Home', path: '/'},
-    {Link: 'About', path: '/about'},
-    {Link: 'Shop', path: '/shop'},
-    {Link: 'Sell Your book', path: '/admin/dashboard'},
-    {Link: 'Blog', path: '/blog'},
- ] 
+  const navItems = [
+    { Link: "Home", path: "/" },
+    { Link: "About", path: "/about" },
+    { Link: "Shop", path: "/shop" },
+    { Link: "Sell Your book", path: "/admin/dashboard" },
+    { Link: "Blog", path: "/blog" },
+  ];
 
-  return <div>Navbar</div>;
+  return (
+    <header>
+      <nav>
+        <div>
+            <Link className="text-2xl font-bold text-blue-700 flex items-center" to='/'><FaBlog className="inline-block"/>Books</Link>
+        </div>
+      </nav>
+    </header>
+  );
 };
 
 export default Navbar;
