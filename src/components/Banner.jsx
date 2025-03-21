293e41b4767ca3b8aa3316bef3dd943b88422759
RuslanGon <ruslan441006@gmail.com>
2025-03-21 16:31:31 +0200

    wer

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,8 +3,11 @@ import React from 'react'
 const Banner = () => {
   return (
     <div className='px-4 lg:px-24 bg-teal-200 flex items-center'>
-        <div>
-            
+        <div className='flex w-full flex-col md:flex-row justify-between items-center gap-12 py-40'>
+            <div>
+                <h2 className='text-5xl font-bold leading-snug'>Buy and Sell Your Books</h2>
+            </div>
+            <div>right side</div>
         </div>
     </div>
   )
