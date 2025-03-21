9df94a44edd9d8fec5a7280e044d13d6ab6bc808
RuslanGon <ruslan441006@gmail.com>
2025-03-21 14:19:31 +0200

    wer

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import { FaBlog } from "react-icons/fa6";
+import { FaBarsStaggered, FaBlog, FaXmark } from "react-icons/fa6";
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -41,7 +41,7 @@ const Navbar = () => {
             to="/" >
             <FaBlog className="inline-block" /> Books
           </Link>
-          <ul className="flex flex-row gap-12">
+          <ul className="md:flex space-x-12 gap-12 hidden">
             {navItems.map(({ link, path }) => (
               <li key={path}>
                 <Link to={path}
@@ -51,6 +51,22 @@ const Navbar = () => {
               </li>
             ))}
           </ul>
+          <div className="space-x-12  lg:flex items-center">
+            <button><FaBarsStaggered className="w-5 hover:text-blue-700 cursor-pointer hidden" /></button>
+          </div>
+          <div className="md:hidden">
+             <button className="text-black focus:outline-none" onClick={toggleMenu}>{isMenuOpen ? <FaXmark className="h-5 w-5 text-black" /> : <FaBarsStaggered className="h-5 w-5 text-black" />}</button>
+          </div>
+        </div>
+        <div className={`space-y-4 px-4`}>
+       { navItems.map(({ link, path }) => (
+              <li key={path}>
+                <Link to={path}
+                  className="block text-base text-black uppercase cursor-pointer hover:text-blue-700"
+                >{link}
+                </Link>
+              </li>
+            ))}
         </div>
       </nav>
     </header>
