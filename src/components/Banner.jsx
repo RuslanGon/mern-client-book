c0366a21fc7ab3d7715faa27caf526abbef56e6a
RuslanGon <ruslan441006@gmail.com>
2025-03-21 17:05:16 +0200

    wre

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -4,14 +4,14 @@ const Banner = () => {
   return (
     <div className="px-4 lg:px-24 bg-teal-200 flex items-center">
       <div className="flex w-full flex-col md:flex-row justify-between items-center gap-12 py-40">
-        <div>
+        <div className='md:w-1/2 space-y-8 h-full'>
           <h2 className="text-5xl font-bold leading-snug text-black">
-            Buy and Sell Your Books
+            Buy and Sell Your Books  
+            <span className='text-blue-700'>  for the best Prices</span>
           </h2>
-          <p>
-            {" "}
+          <p className='md:w-4/5'>
             Discover new horizons with your favorite books! Buy rare editions,
-            sell pre-loved books, and share knowledge with others. Build your
+            sell  preloved books, and share knowledge with others. Build your
             perfect book collection today!
           </p>
           <div>
@@ -22,7 +22,7 @@ const Banner = () => {
               placeholder="Search a book"
               className="px-2 py-2 w-64 border border-gray-400 rounded-lg focus:outline-none focus:ring-2 focus:ring-teal-500"
             />
-            <button className='bg-teal-500 px-6 py-2 text-white font-medium hover:bg-black transition-all ease-in duration-200 rounded-lg'>Search</button>
+            <button className='bg-blue-700 px-6 py-2 text-white font-medium hover:bg-black transition-all ease-in duration-200 rounded-lg'>Search</button>
           </div>
         </div>
         <div>right side</div>
