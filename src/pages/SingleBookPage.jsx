15780e681de96bddcf2f47f2008cfb0be4a22cb4
RuslanGon <ruslan441006@gmail.com>
2025-03-24 17:40:57 +0200

    wer

--- src/pages/SingleBookPage.jsx
+++ src/pages/SingleBookPage.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
 
 const SingleBookPage = () => {
-  return (
-    <div>SingleBookPage</div>
-  )
-}
+  const { id } = useParams();
+  const [book, setBook] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-export default SingleBookPage
+  useEffect(() => {
+    const fetchBook = async () => {
+      try {
+        const response = await axios.get(`http://localhost:5000/book/${id}`);
+        setBook(response.data);
+      } catch (error) {
+        console.log(error);
+        setError("Ошибка загрузки книги");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchBook();
+  }, [id]);
+
+  if (loading) return <p>Загрузка...</p>;
+  if (error) return <p>{error}</p>;
+
+  return (
+    <div>
+      <h2>{book.bookTitle}</h2>
+      <img src={book.imageURL} alt={book.bookTitle} />
+      <p>{book.bookDescription}</p>
+    </div>
+  );
+};
+
+export default SingleBookPage;
