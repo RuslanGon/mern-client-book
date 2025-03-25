c967ba4252bd17881483baa5cbb26e9eca1fa390
RuslanGon <ruslan441006@gmail.com>
2025-03-25 12:07:32 +0200

    wer

--- src/pages/RewievPage.jsx
+++ src/pages/RewievPage.jsx
@@ -1,7 +1,9 @@
 
 const RewievPage = () => {
   return (
-    <div>RewievPage</div>
+    <div className="my-12 px-4 lg:px-24">
+        <h2 className="text-5xl font-bold text-center mb-10 leading-snug">Our Customers</h2>
+    </div>
   )
 }
 
