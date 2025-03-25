1a90801a569eb19ac61cc788d87e9ead2a8aa8e4
RuslanGon <ruslan441006@gmail.com>
2025-03-25 11:19:12 +0200

    wer

--- src/pages/FavBook.jsx
+++ src/pages/FavBook.jsx
@@ -32,7 +32,7 @@ const FavBook = () => {
           </div>
         </div>
         <div className="mt-auto">
-          <Link to='/shop'>
+          <Link className='block' to='/shop'>
             <button className='bg-blue-700 text-white font-semibold px-5 py-2 rounded hover:bg-black transition-all duration-300 w-full md:w-auto cursor-pointer'>
               Explore More
             </button>
