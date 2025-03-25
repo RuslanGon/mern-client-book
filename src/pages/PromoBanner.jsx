49d148d0ddf80d2eb9ec17f7e0fee3cffdc67289
RuslanGon <ruslan441006@gmail.com>
2025-03-25 11:39:49 +0200

    wwe

--- src/pages/PromoBanner.jsx
+++ src/pages/PromoBanner.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import BooksImg from '../assets/awardbooks.png'
 
 const PromoBanner = () => {
   return (
-    <div>PromoBanner</div>
-  )
-}
+    <div className="mt-16 py-12 bg-teal-100 px-4 lg:px-24">
+      <div>
+        <div className="md:w-1/2">
+          <h2 className="text-4xl font-bold mb-6 leading-snug">
+            2025 National Book Awards for Fiction Shortlist
+          </h2>
+          <Link className="block" to="/shop">
+            <button className="bg-blue-700 text-white font-semibold px-5 py-2 rounded hover:bg-black transition-all duration-300 w-full md:w-auto cursor-pointer">
+              Get Promo Code
+            </button>
+          </Link>
+        </div>
+        <div>
+            <img src={BooksImg} alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default PromoBanner
+export default PromoBanner;
