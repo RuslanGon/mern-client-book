43447066b6155b8bb187630ce1da409c7cc5d989
RuslanGon <ruslan441006@gmail.com>
2025-03-26 12:32:00 +0200

    wer

--- src/pages/ShopPage.jsx
+++ src/pages/ShopPage.jsx
@@ -22,7 +22,7 @@ export const ShopPage = () => {
   }, []);
 
   return (
-    <div className="px-4 lg:px-24  bg-teal-200">
+    <div className="px-4 lg:px-24 bg-teal-200">
       <h2 className="text-5xl font-bold text-center mb-10 pt-10">All Books are here</h2>
 
       {loading && <p className="text-center text-lg">Загрузка...</p>}
@@ -44,11 +44,12 @@ export const ShopPage = () => {
               <p className="text-gray-600">by {book.authorName}</p>
               <p className="text-sm text-gray-500">{book.category}</p>
               <p
-                className="text-gray-700 mt-2 text-sm text-center line-clamp-7"
-                title={book.bookDescription} 
+                className="text-gray-700 mt-2 text-sm text-center line-clamp-4"
+                title={book.bookDescription}
               >
                 {book.bookDescription}
               </p>
+              <button className="bg-blue-700 mt-4 px-16 py-2 text-white font-medium hover:bg-black transition-all ease-in duration-200 rounded-lg cursor-pointer">Buy now</button>
             </div>
           ))}
         </div>
