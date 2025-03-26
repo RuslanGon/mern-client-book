e68e489dcc4bc0fff0ce81f4aa58d4191af6f80a
RuslanGon <ruslan441006@gmail.com>
2025-03-26 11:17:43 +0200

    wer

--- src/pages/ShopPage.jsx
+++ src/pages/ShopPage.jsx
@@ -1,25 +1,28 @@
-import { NavLink, Routes, Route, Outlet } from "react-router-dom";
-import { About } from "../components/About.jsx";
-import { Blog } from "../components/Blog.jsx";
+
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 export const ShopPage = () => {
-  return (
-    <>
-      <nav>
-        <NavLink to="/shop/about" className="mr-4 hidden md:inline-block">
-          About
-        </NavLink>
-        <NavLink to="/shop/blog" className="mr-4 hidden md:inline-block">
-          Blog
-        </NavLink>
-      </nav>
 
-      <Routes>
-        <Route path="about" element={<About />} />
-        <Route path="blog" element={<Blog />} />
-      </Routes>
-      
-      <Outlet /> 
-    </>
+const [books, setBooks] = useState([])
+
+
+useEffect(() => {
+  const fetchBooksShop = async () => {
+    try {
+      const { data } = await axios.get("http://localhost:5000/all-books");
+      setBooks(data);
+    } catch (error) {
+      console.error("Ошибка при загрузке книг:", error);
+    }
+  };
+  fetchBooksShop();
+}, []);
+
+  return (
+   <div className="mt-28 px-4 lg:px-24">
+    <h2>All Books are here</h2>
+
+   </div>
   );
 };
