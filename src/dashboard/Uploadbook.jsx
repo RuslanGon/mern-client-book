23ebed3ee67caaee44e83ed8e0c1a43f0e024ff1
RuslanGon <ruslan441006@gmail.com>
2025-03-26 18:25:34 +0200

    wer

--- src/dashboard/Uploadbook.jsx
+++ src/dashboard/Uploadbook.jsx
@@ -3,54 +3,23 @@ const UploadBook = () => {
     <div className="px-4 my-5">
       <h2 className="mbb-8 text-3xl font-bold">Upload a Book</h2>
       <form className="flex lg:w-[900px] flex-col flex-wrap gap-4">
-        <div>
+        <div className="lg:w-1/2">
           <label
-            htmlFor="email1"
+            htmlFor="bookTitle"
+            value='Book Title'
             className="mb-2 block font-medium text-gray-700 dark:text-gray-300"
           >
             Your email
           </label>
           <input
-            type="email"
-            id="email1"
+            type="bookTitle"
+            id="bookTitle"
+            name="bookTitle"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             placeholder="name@example.com"
             required
           />
         </div>
-        <div>
-          <label
-            htmlFor="password1"
-            className="mb-2 block font-medium text-gray-700 dark:text-gray-300"
-          >
-            Your password
-          </label>
-          <input
-            type="password"
-            id="password1"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            required
-          />
-        </div>
-        <div className="flex items-center gap-2">
-          <input
-            type="checkbox"
-            id="remember"
-            className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-          />
-          <label
-            htmlFor="remember"
-            className="font-medium text-gray-900 dark:text-gray-300"
-          >
-            Remember me
-          </label>
-        </div>
-        <button
-          type="submit"
-          className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        >
-          Submit
-        </button>
       </form>
     </div>
   );
