37db97b0a80a3e1bab8e6ca15a3815f76f322bbb
RuslanGon <ruslan441006@gmail.com>
2025-03-27 16:22:53 +0200

    wer

--- src/dashboard/ManageBooks.jsx
+++ src/dashboard/ManageBooks.jsx
@@ -1,8 +1,86 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const ManageBooks = () => {
-  return (
-    <div>ManageBooks</div>
-  )
-}
+  const [allBooks, setAllBooks] = useState([]); 
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
 
-export default ManageBooks
+  useEffect(() => {
+    const fetchManageBooks = async () => {
+      try {
+        const { data } = await axios.get("http://localhost:5000/all-books");
+        setAllBooks(data);
+      } catch (error) {
+        console.error("Error loading books:", error);
+        setError("Error loading books.");
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchManageBooks();
+  }, []);
+
+  return (
+    <div className="px-4 my-5">
+      <h2 className="mb-8 text-3xl font-bold">Manage Your Books</h2>
+      {loading && <p>Loading...</p>}
+      {error && <p>{error}</p>}
+      <table className="lg:w-[1000px]">
+        <thead className="bg-gray-50 dark:bg-gray-700">
+          <tr>
+            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              No.
+            </th>
+            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              Book Name
+            </th>
+            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              Author Name
+            </th>
+            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              Category
+            </th>
+            <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+              Prices
+            </th>
+            <th className="relative px-6 py-3">
+              <span className="text-xs text-gray-500 uppercase">Edit or Manage</span>
+            </th>
+          </tr>
+        </thead>
+        <tbody className="bg-white divide-y divide-gray-200 dark:bg-gray-800 dark:divide-gray-700">
+          {allBooks.map((book, index) => (
+            <tr key={book._id}>
+              <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 dark:text-white">
+                {index + 1}
+              </td>
+              <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 dark:text-white">
+                {book.bookTitle} {/* Используем bookTitle */}
+              </td>
+              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300">
+                {book.authorName} {/* Используем authorName */}
+              </td>
+              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300">
+                {book.category}
+              </td>
+              <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300">
+                ${book.price}
+              </td>
+              <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                <a
+                  href="#"
+                  className="text-cyan-600 hover:underline dark:text-cyan-500"
+                >
+                  Edit
+                </a>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default ManageBooks;
